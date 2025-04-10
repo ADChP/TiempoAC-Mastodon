dad88c0e38848f340c0265b909f1e73d44c0eef6
Andres <chavacr93@gmail.com>
2025-04-09 18:48:32 -0600

    Actualización de enlaces de fuentes en App 1 y App 2. Inclusión de App 3 a producción. Encabezados de introducción en App 1 y App 2.

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
+'''
+Tiempo América Central - App # 1
+Script hecho por ADChP
+@tunkuluchu@mastodon.cr
+San José, Costa Rica.
+Marzo, 2025
+'''
+
 import logging
 import requests
 import time
@@ -5,7 +13,7 @@ import time
 #------------ CONFIG ------------------
 logging.basicConfig(filename='log_app.log', encoding='utf-8', level=logging.DEBUG,
                     format='%(asctime)s - %(message)s', datefmt='%d/%m/%Y %I:%M:%S %p')
-imagen = 'https://cdn.star.nesdis.noaa.gov/GOES16/ABI/SECTOR/cam/GEOCOLOR/1000x1000.jpg'
+imagen = 'https://cdn.star.nesdis.noaa.gov/GOES19/ABI/SECTOR/cam/GEOCOLOR/1000x1000.jpg'
 api_text = 'https://bots.fedi.cr/api/v1/statuses'
 api_imagen = 'https://bots.fedi.cr/api/v2/media'
 encabezados = {
@@ -36,7 +44,7 @@ req = requests.request('POST',api_imagen,headers=encabezados,files=imagen, data=
 if req.status_code == 200:
     id_imagen = req.json()['id']
     toot = {
-        'status': '-* Publicación automática *-\n\nCondiciones atmosféricas actuales en #AméricaCentral.\n\nFuente: National Oceanic and Atmospheric Administration.\n\n#GOES16 #NOAA',
+        'status': '-* Publicación automática *-\n\nCondiciones atmosféricas actuales en #AméricaCentral.\n\nFuente: National Oceanic and Atmospheric Administration.\n\n#GOES19 #NOAA',
         'media_ids[]': [id_imagen]
     }
     
@@ -55,7 +63,7 @@ elif req.status_code == 202:
     
     if req.status_code == 200:
         toot = {
-        'status': '-* Publicación automática *-\n\nCondiciones atmosféricas actuales en #AméricaCentral.\n\nFuente: National Oceanic and Atmospheric Administration.\n\n#GOES16 #NOAA',
+        'status': '-* Publicación automática *-\n\nCondiciones atmosféricas actuales en #AméricaCentral.\n\nFuente: National Oceanic and Atmospheric Administration.\n\n#GOES19 #NOAA',
         'media_ids[]': [id_imagen]
         }
         
